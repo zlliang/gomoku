555a8a3ef3198a313d089c290a027705ea80103e
Zilong Liang <zlliang15@fudan.edu.cn>
2018-06-16 11:50:15 +0800

    Update framework

--- evaluate.py
+++ evaluate.py
@@ -10,3 +10,14 @@ def evaluate(board):
         score: Float number of current board score
     """
     raise NotImplementedError("Evaluate function has not been implemented!")
+    eval_func = [eval_1, eval_2]
+    score = [func(board) for func in eval_func]
+    score = sum(score)
+    return score
+
+# Helper functions
+def eval_1(board):
+    return 0
+
+def eval_2(board):
+    return 0
--- util.py
+++ util.py
@@ -1,11 +1,25 @@
 class Board:
-    def __init__(self, board=None):
-        self.board = None
-        self._pattern_cache = None
-    
-    def __getitem__(self, indices):
-        x, y = indices
-        return self.board[x][y]
+    def __init__(self, scale=10):
+        self._board = [[0 for i in range(scale)] for j in range(scale)]
+        self._pattern_cache = self._init_pattern()
     
     def pattern(self):
         raise NotImplementedError("Pattern getter has not been implemented!")
+    
+    def _update_pattern(self):
+        raise NotImplementedError()
+    
+    def _init_pattern(self):
+        raise NotImplementedError()
+    
+    def __getitem__(self, indices):
+        x, y = indices
+        return self._board[x][y]
+    
+    def __setitem__(self, indices, value):
+        x, y = indices
+        self._board[x][y] = value
+        self._update_pattern()
+    
+    def __repr__(self):
+        raise NotImplementedError()
