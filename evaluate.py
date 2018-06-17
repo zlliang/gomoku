355584ea2ba3904ecbee4a0f8e91c71daec3eb04
Zilong Liang <zlliang15@fudan.edu.cn>
2018-06-17 12:14:08 +0800

    Update example evaluate functions

--- evaluate.py
+++ evaluate.py
@@ -9,15 +9,29 @@ def evaluate(board):
     -------
         score: Float number of current board score
     """
-    raise NotImplementedError("Evaluate function has not been implemented!")
-    eval_func = [eval_1, eval_2]
+    # raise NotImplementedError("Evaluate function has not been implemented!")
+    eval_func = [huo4, huo3]
     score = [func(board) for func in eval_func]
     score = sum(score)
     return score
 
 # Helper functions
-def eval_1(board):
-    return 0
+def huo4(board):
+    base = 90
+    score = 0
+    for pattern in board.pattern('6'):
+        if pattern == [0, 1, 1, 1, 1, 0]:
+            score += base
+        elif pattern == [0, 2, 2, 2, 2, 0]:
+            score -= base
+    return score
 
-def eval_2(board):
-    return 0
+def huo3(board):
+    base = 50
+    score = 0
+    for pattern in board.pattern('5'):
+        if pattern == [0, 1, 1, 1, 0]:
+            score += base
+        elif pattern == [0, 2, 2, 2, 0]:
+            score -= base
+    return score
