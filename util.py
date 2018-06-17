1425314b7d5b101dcbdec7ec9e75bd7722f54ad7
Zilong Liang <zlliang15@fudan.edu.cn>
2018-06-17 11:48:46 +0800

    Update Board class

--- util.py
+++ util.py
@@ -1,25 +1,44 @@
 class Board:
-    def __init__(self, scale=10):
-        self._board = [[0 for i in range(scale)] for j in range(scale)]
-        self._pattern_cache = self._init_pattern()
+    def __init__(self, board=None, scale=10):
+        if not board:
+            self._board = [[0 for i in range(scale)] for j in range(scale)]
+        else:
+            self._board = board
+    #     self._pattern_cache = self._init_pattern()
     
-    def pattern(self):
-        raise NotImplementedError("Pattern getter has not been implemented!")
+    # def pattern(self):
+    #     raise NotImplementedError("Pattern getter has not been implemented!")
     
-    def _update_pattern(self):
-        raise NotImplementedError()
+    # def _update_pattern(self):
+    #     raise NotImplementedError()
     
-    def _init_pattern(self):
-        raise NotImplementedError()
+    # def _init_pattern(self):
+    #     raise NotImplementedError()
     
     def __getitem__(self, indices):
-        x, y = indices
-        return self._board[x][y]
+        y, x = indices
+        if not isinstance(x, slice) and not isinstance(y, slice):
+            return self._board[x][y]
+        if isinstance(y, slice):
+            subset = [row[y] for row in self._board]
+        else:
+            subset = [[row[y]] for row in self._board]
+        subset = subset[x]
+        if isinstance(subset, list):
+            return Board(subset)
+        else:
+            return self._board[x][y]
     
     def __setitem__(self, indices, value):
-        x, y = indices
+        y, x = indices
+        if isinstance(x, slice) or isinstance(y, slice):
+            raise ValueError("Trying to assign multiple values to the board!")
         self._board[x][y] = value
-        self._update_pattern()
+        # self._update_pattern()
     
     def __repr__(self):
-        raise NotImplementedError()
+        if isinstance(self._board[0], list):
+            l = [str(row) for row in self._board]
+            return '\n'.join(l)
+        else:
+            return str(self._board)
